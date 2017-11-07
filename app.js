d457be36a1244641e875ff7f3f4259254dffc382
lawyatt88 <lemonawyatt@gmail.com>
2017-11-07 12:00:09 -0500

    added nunjucks functionality

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
-const volleyball = require('volleyball')
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 app.use(volleyball)
 
 app.listen(3000, function () {
     console.log('Listening on port 3000!')
   })
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render)
+nunjucks.configure('views', { noCache: true });
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.get("/" ,function(req, res, next) {
+    res.render( 'index', {title: 'Hall of Fame', people: people}) 
+});
 
 // app.use("/", function (req, res, next) {
 // // Remember - one request, one response!
@@ -18,3 +28,6 @@ app.listen(3000, function () {
 //     //console.log(req.method, req.url, res.statusCode);
 //     console.log('you reached the special area');
 // })
+
+
+
