528e25dbefea2cdab3b68fcbf72c8aa6fc8ebcbf
lawyatt88 <lemonawyatt@gmail.com>
2017-11-07 11:13:24 -0500

    added volleyball

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const volleyball = require('volleyball')
+
+app.use(volleyball)
 
 app.listen(3000, function () {
     console.log('Listening on port 3000!')
   })
 
-app.get("/", function (req, res, next) {
-// Remember - one request, one response!
-    res.send('<h1>Hello world!</h1>')
-})
+// app.use("/", function (req, res, next) {
+// // Remember - one request, one response!
+//     console.log(req.method, req.url, res.statusCode);
+//     next();
+// })
+
+// app.use("/special", function (req, res, next) {
+//     //console.log(req.method, req.url, res.statusCode);
+//     console.log('you reached the special area');
+// })
